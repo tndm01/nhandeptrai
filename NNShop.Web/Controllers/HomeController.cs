390879f9432b9773fd9188a70f82be3fa1bb096c
ThanhNhan <ThanhNhan@THANHNHAN-PC>
2017-01-14 17:33:44 -0800

    Bingding thành phần dùng chung bằng PartialView

--- NNShop.Web/Controllers/HomeController.cs
+++ NNShop.Web/Controllers/HomeController.cs
@@ -3,11 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using NNShop.Model.Models;
+using NNShop.Service;
+using NNShop.Web.Models;
 
 namespace NNShop.Web.Controllers
 {
     public class HomeController : Controller
     {
+        IProductCategoryService _productCategoryService;
+
+        public HomeController(IProductCategoryService productCategoryService)
+        {
+            this._productCategoryService = productCategoryService;
+        }
+
         public ActionResult Index()
         {
             return View();
@@ -45,7 +56,10 @@ namespace NNShop.Web.Controllers
         [ChildActionOnly]
         public ActionResult Category()
         {
-            return PartialView();
+            var model = _productCategoryService.GetAll();
+            var listProductCategoryViewModel = Mapper.Map<IEnumerable<ProductCategory>, IEnumerable<ProductCategoryViewModel>>(model);
+
+            return PartialView(listProductCategoryViewModel);
         }
     }
 }
