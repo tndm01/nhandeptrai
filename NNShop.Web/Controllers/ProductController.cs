aebd8b15c005a0b4675f0c584dbcdca1872b08fb
ThanhNhan <ThanhNhan@THANHNHAN-PC>
2017-01-15 22:35:00 -0800

    Tạo trang danh sách sản phẩm và phân trang

--- NNShop.Web/Controllers/ProductController.cs
+++ NNShop.Web/Controllers/ProductController.cs
@@ -3,20 +3,49 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AutoMapper;
+using NNShop.Common;
+using NNShop.Model.Models;
+using NNShop.Service;
+using NNShop.Web.Infrastructure.Core;
+using NNShop.Web.Models;
 
 namespace NNShop.Web.Controllers
 {
     public class ProductController : Controller
     {
+        IProductService _productService;
+        IProductCategoryService _productCategoryService;
+        public ProductController(IProductService productService, IProductCategoryService productCategoryService)
+        {
+            _productService = productService;
+            _productCategoryService = productCategoryService;
+        }
         // GET: Product
         public ActionResult Detail(int id)
         {
             return View();
         }
 
-        public ActionResult Category(int id)
+        public ActionResult Category(int id, int page = 1)
         {
-            return View();
+            int pageSize = int.Parse(ConfigHelper.GetByKey("pageSize"));
+            int totalRow = 0;
+            var productModel = _productService.GetListProductByCategoryIdPaging(id, page, pageSize ,out totalRow);
+            var productViewModel = Mapper.Map<IEnumerable<Product>,IEnumerable<ProductViewModel>>(productModel);
+            int totalPage = (int)Math.Ceiling((double)totalRow / pageSize);
+
+            var category = _productCategoryService.GetById(id);
+            ViewBag.Category = Mapper.Map<ProductCategory, ProductCategoryViewModel>(category);
+            var paginationSet = new PaginationSet<ProductViewModel>()
+            {
+                Items = productViewModel,
+                MaxPage = int.Parse(ConfigHelper.GetByKey("MaxPage")),
+                Page = page,
+                TotalCount = totalRow,
+                TotalPages = totalPage
+            };
+            return View(paginationSet);
         }
     }
 }
